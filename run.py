aa2b06b52d67988f9d7465fd4353d00c7c239ace
ntharun515-bit <ntharun515-bit@users.noreply.github.com>
2026-06-02 16:56:13 +0530

    Update run port to 5000 and load dotenv in init_db

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from backend.app import app
 from backend.extensions import socketio
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5001, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
